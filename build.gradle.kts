d8492ec44b0d1f3b9fe9e6cd23c98667898fb270
Michael Willis <michael.willis@digital.justice.gov.uk>
2022-01-07 14:43:49 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
+  kotlin("plugin.spring") version "1.6.0"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "16"
+    }
+  }
+}
