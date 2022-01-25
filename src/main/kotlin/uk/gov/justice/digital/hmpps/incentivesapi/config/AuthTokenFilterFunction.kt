7c2b34d0b89d1196f9b20da6b7ba4e9960cd0f54
mjwillis <michael.willis@digital.justice.gov.uk>
2022-01-25 20:40:43 +0000

    INC-122 Auto populate auth headers in context
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/AuthTokenFilterFunction.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/AuthTokenFilterFunction.kt
@@ -1,14 +1,20 @@
 package uk.gov.justice.digital.hmpps.incentivesapi.config
 
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeFunction
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
 import reactor.core.CoreSubscriber
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.util.context.Context
+
 
 class AuthTokenFilterFunction : ExchangeFilterFunction {
 
@@ -29,6 +35,19 @@ class AuthTokenResponseMono(
     val mutatedRequest = requestBuilder.build()
     next.exchange(mutatedRequest).subscribe(subscriber)
   }
+}
+
+@Component
+@Order(1)
+class UserContextFilter : WebFilter {
+  override fun filter(serverWebExchange: ServerWebExchange, webFilterChain: WebFilterChain): Mono<Void> {
+    val authHeader = serverWebExchange.request.headers[HttpHeaders.AUTHORIZATION]?.firstOrNull()?:"none"
+
+    return webFilterChain.filter(serverWebExchange).contextWrite {
+      transferAuthHeader(authHeader, it)
+    }
+  }
+
 }
 
 fun <T> Flux<T>.withToken(authToken: String): Flux<T> =
