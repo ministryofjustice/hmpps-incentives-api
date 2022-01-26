44680afbd18e6616bc16aa9747deb247a1c2aacc
mjwillis <michael.willis@digital.justice.gov.uk>
2022-01-26 11:15:38 +0000

    INC-122 Auto populate auth headers in context
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/WebClientConfiguration.kt
@@ -3,7 +3,10 @@ package uk.gov.justice.digital.hmpps.incentivesapi.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
 
 @Configuration
 class WebClientConfiguration(
@@ -20,8 +23,10 @@ class WebClientConfiguration(
 
   @Bean
   fun prisonWebClient(): WebClient {
+    val httpClient = HttpClient.create().responseTimeout(Duration.ofMinutes(2))
     return WebClient.builder()
       .baseUrl(prisonRootUri)
+      .clientConnector(ReactorClientHttpConnector(httpClient))
       .filter(AuthTokenFilterFunction())
       .build()
   }
