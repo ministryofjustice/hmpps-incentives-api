47519d1e1b55d1fb5d20879154d3900676a38a7e
mjwillis <michael.willis@digital.justice.gov.uk>
2022-01-31 10:24:33 +0000

    INC-156 Upgrade hmpps-gradle-springboot plugin to 4.0.2
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.2"
   kotlin("plugin.spring") version "1.6.10"
 }
 
@@ -13,9 +13,9 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.5")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 
