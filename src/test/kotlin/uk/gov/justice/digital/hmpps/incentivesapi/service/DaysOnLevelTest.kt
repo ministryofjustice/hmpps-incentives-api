70a626314966dbaee71e71a50e33f0e0a1049af0
mjwillis <michael.willis@digital.justice.gov.uk>
2022-02-25 17:15:46 +0000

    INC-253 Switch to IEP Levels from Incentives DB
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/DaysOnLevelTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/DaysOnLevelTest.kt
@@ -4,8 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
-import uk.gov.justice.digital.hmpps.incentivesapi.jpa.repository.IepLevelRepository
-import uk.gov.justice.digital.hmpps.incentivesapi.jpa.repository.IepPrisonRepository
 import java.time.LocalDateTime
 
 class DaysOnLevelTest {
