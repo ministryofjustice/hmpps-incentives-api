e6b0e7e81ccb3274b93b5df9b4a8e31b47f10ac3
Neil Mendum <nmendum@gmail.com>
2022-03-28 18:44:34 +0100

    INC-405: upgrade hmpps-gradle-springboot plugin (#55)
    * INC-405: upgrade hmpps-gradle-springboot plugin to 4.1.1; also bump other dependencies, gradle-wrapper, generic helm charts; readme tidy up
    
    * INC-405: upgrade hmpps-gradle-springboot plugin to 4.1.2-beta; docker compose and readme tidyup
    
    * INC-405: use latest hmpps-gradle-springboot beta version

--- README.md
+++ README.md
@@ -5,10 +5,7 @@
 [![Runbook](https://img.shields.io/badge/runbook-view-172B4D.svg?logo=confluence)](https://dsdmoj.atlassian.net/wiki/spaces/NOM/pages/1739325587/DPS+Runbook)
 [![API docs](https://img.shields.io/badge/API_docs_-view-85EA2D.svg?logo=swagger)](https://incentives-api-dev.hmpps.service.justice.gov.uk/webjars/swagger-ui/index.html?configUrl=/v3/api-docs)
 
-
 **Incentives Domain Microservice to own the incentives data for prisoners**
-
-
 
 ## Running locally
 
@@ -19,11 +16,9 @@ For running locally against docker instances of the following services:
 
 `docker-compose up hmpps-auth prison-api`
 
-
-Running all services including this service
+## Running all services including this service
 
 `docker-compose up`
-
 
 ## Running locally against T3 test services
 
@@ -32,6 +27,12 @@ This is straight-forward as authentication is delegated down to the calling serv
 API_BASE_URL_OAUTH=https://sign-in-dev.hmpps.service.justice.gov.uk/auth
 API_BASE_URL_PRISON=https://api-dev.prison.service.justice.gov.uk
 ```
+
+## Running integration tests
+
+Before running integration tests you need to start a localstack instance
+
+`docker-compose up localstack`
 
 ### Runbook
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.2-beta-2"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
 }
@@ -21,23 +21,23 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.6")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.2")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.0")
 
-  implementation("org.flywaydb:flyway-core:8.5.1")
-  implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
+  implementation("org.flywaydb:flyway-core:8.5.4")
+  implementation("com.vladmihalcea:hibernate-types-52:2.14.1")
   runtimeOnly("io.r2dbc:r2dbc-postgresql")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
-  runtimeOnly("org.postgresql:postgresql:42.3.2")
+  runtimeOnly("org.postgresql:postgresql:42.3.3")
   implementation("com.zaxxer:HikariCP:5.0.1")
 
   implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.6")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-  implementation("io.opentelemetry:opentelemetry-api:1.11.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+  implementation("io.opentelemetry:opentelemetry-api:1.12.0")
 
   implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("org.apache.commons:commons-text:1.9")
@@ -48,10 +48,10 @@ dependencies {
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-  testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.3.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.30")
+  testImplementation("org.mockito:mockito-inline:4.4.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.31")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
