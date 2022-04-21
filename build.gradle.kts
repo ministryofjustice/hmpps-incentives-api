1821c3cb6e5cd7af84f07fedc94502286a37e5ac
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2022-04-21 09:15:10 +0100

    INC-493: bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.3"
-  kotlin("plugin.spring") version "1.6.20"
-  kotlin("plugin.jpa") version "1.6.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5-beta"
+  kotlin("plugin.spring") version "1.6.21"
+  kotlin("plugin.jpa") version "1.6.21"
 }
 
 dependencyCheck {
@@ -26,18 +26,18 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1")
 
-  implementation("org.flywaydb:flyway-core:8.5.7")
-  implementation("com.vladmihalcea:hibernate-types-52:2.15.1")
+  implementation("org.flywaydb:flyway-core:8.5.8")
+  implementation("com.vladmihalcea:hibernate-types-52:2.16.1")
   runtimeOnly("io.r2dbc:r2dbc-postgresql")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
-  runtimeOnly("org.postgresql:postgresql:42.3.3")
+  runtimeOnly("org.postgresql:postgresql:42.3.4")
   implementation("com.zaxxer:HikariCP:5.0.1")
 
   implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.7")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
-  implementation("io.opentelemetry:opentelemetry-api:1.12.0")
+  implementation("io.opentelemetry:opentelemetry-api:1.13.0")
 
   implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("org.apache.commons:commons-text:1.9")
@@ -50,13 +50,13 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.4.0")
+  testImplementation("org.mockito:mockito-inline:4.5.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.32")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.testcontainers:localstack:1.16.3")
-  testImplementation("org.testcontainers:postgresql:1.16.3")
+  testImplementation("org.testcontainers:localstack:1.17.1")
+  testImplementation("org.testcontainers:postgresql:1.17.1")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
 }
