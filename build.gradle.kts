11a3108d30d56682966d847f7d91a4ea508093b8
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2022-05-04 16:30:29 +0100

    INC-545: bump gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.6"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.jpa") version "1.6.21"
 }
@@ -26,15 +26,15 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1")
 
-  implementation("org.flywaydb:flyway-core:8.5.8")
-  implementation("com.vladmihalcea:hibernate-types-52:2.16.1")
+  implementation("org.flywaydb:flyway-core:8.5.10")
+  implementation("com.vladmihalcea:hibernate-types-52:2.16.2")
   runtimeOnly("io.r2dbc:r2dbc-postgresql")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.3.4")
   implementation("com.zaxxer:HikariCP:5.0.1")
 
-  implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.7")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
   implementation("io.opentelemetry:opentelemetry-api:1.13.0")
@@ -50,7 +50,7 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.5.0")
+  testImplementation("org.mockito:mockito-inline:4.5.1")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.32")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
