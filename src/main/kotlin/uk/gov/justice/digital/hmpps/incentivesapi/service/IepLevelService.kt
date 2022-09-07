f406b729226ad5674d46735cef7254c0f62e336c
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2022-09-07 10:30:01 +0100

    Removed unnecessary `coroutineScope` in IepLevelService

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/IepLevelService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/IepLevelService.kt
@@ -1,11 +1,8 @@
 package uk.gov.justice.digital.hmpps.incentivesapi.service
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.withContext
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.incentivesapi.jpa.repository.IepLevelRepository
@@ -19,22 +16,20 @@ class IepLevelService(
 
   @Transactional(readOnly = true)
   suspend fun getIepLevelsForPrison(prisonId: String): List<IepLevel> {
-    return coroutineScope {
-      val iepLevelMap = iepLevelRepository.findAll().toList().associateBy { it.iepCode }
+    val iepLevelMap = iepLevelRepository.findAll().toList().associateBy { it.iepCode }
 
-      withContext(Dispatchers.Default) {
-        iepPrisonRepository.findAllByPrisonIdAndActiveIsTrue(prisonId)
-          .filter { iepLevelMap[it.iepCode]?.active == true }
-          .map {
-            val iepLevel = iepLevelMap[it.iepCode]!!
-            IepLevel(
-              iepLevel = it.iepCode,
-              iepDescription = iepLevel.iepDescription,
-              sequence = iepLevel.sequence,
-              default = it.defaultIep
-            )
-          }
-      }.toList().sortedWith(compareBy(IepLevel::sequence))
-    }
+    return iepPrisonRepository.findAllByPrisonIdAndActiveIsTrue(prisonId)
+      .filter { iepLevelMap[it.iepCode]?.active == true }
+      .map {
+        val iepLevel = iepLevelMap[it.iepCode]!!
+        IepLevel(
+          iepLevel = it.iepCode,
+          iepDescription = iepLevel.iepDescription,
+          sequence = iepLevel.sequence,
+          default = it.defaultIep
+        )
+      }
+      .toList()
+      .sortedWith(compareBy(IepLevel::sequence))
   }
 }
