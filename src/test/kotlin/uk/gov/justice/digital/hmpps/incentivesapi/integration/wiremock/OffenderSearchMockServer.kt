a4f9dffea4b28d755e3700778baf08afdfcfdcc8
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-11-04 15:28:35 +0000

    Have offender search return the same set of prisoners in wiremock stubs as prison-api …because the reviews service will need to call _both_ apis hence the booking ids & prisoner numbers must match up

--- src/test/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/integration/wiremock/OffenderSearchMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/integration/wiremock/OffenderSearchMockServer.kt
@@ -27,56 +27,104 @@ class OffenderSearchMockServer : WireMockServer(WIREMOCK_PORT) {
 
   fun stubFindOffenders(prisonId: String) {
     val mapper = jacksonObjectMapper().findAndRegisterModules()
+    val offenders = listOf(
+      // <editor-fold desc="mocked offenders">
+      OffenderSearchPrisoner(
+        prisonerNumber = "A1234AA",
+        bookingId = "1234134",
+        firstName = "JOHN",
+        middleNames = null,
+        lastName = "SMITH",
+        status = "ACTIVE IN",
+        inOutStatus = "IN",
+        dateOfBirth = LocalDate.parse("1971-07-01"),
+        receptionDate = LocalDate.parse("2020-07-01"),
+        prisonId = prisonId,
+        prisonName = "$prisonId prison",
+        cellLocation = "1-1-001",
+        locationDescription = "$prisonId prison",
+        alerts = listOf(
+          OffenderSearchPrisonerAlert(
+            alertType = "H",
+            alertCode = "HA",
+            active = true,
+            expired = false,
+          ),
+        ),
+      ),
+      OffenderSearchPrisoner(
+        prisonerNumber = "A1234AB",
+        bookingId = "1234135",
+        firstName = "DAVID",
+        middleNames = null,
+        lastName = "WHITE",
+        status = "ACTIVE IN",
+        inOutStatus = "IN",
+        dateOfBirth = LocalDate.parse("1970-03-01"),
+        receptionDate = LocalDate.parse("2020-07-01"),
+        prisonId = prisonId,
+        prisonName = "$prisonId prison",
+        cellLocation = "1-1-002",
+        locationDescription = "$prisonId prison",
+        alerts = emptyList(),
+      ),
+      OffenderSearchPrisoner(
+        prisonerNumber = "A1234AC",
+        bookingId = "1234136",
+        firstName = "TREVOR",
+        middleNames = null,
+        lastName = "LEE",
+        status = "ACTIVE IN",
+        inOutStatus = "IN",
+        dateOfBirth = LocalDate.parse("1970-03-02"),
+        receptionDate = LocalDate.parse("2020-07-01"),
+        prisonId = prisonId,
+        prisonName = "$prisonId prison",
+        cellLocation = "1-1-003",
+        locationDescription = "$prisonId prison",
+        alerts = emptyList(),
+      ),
+      OffenderSearchPrisoner(
+        prisonerNumber = "A1234AD",
+        bookingId = "1234137",
+        firstName = "ANTHONY",
+        middleNames = null,
+        lastName = "DAVIES",
+        status = "ACTIVE IN",
+        inOutStatus = "IN",
+        dateOfBirth = LocalDate.parse("1970-03-03"),
+        receptionDate = LocalDate.parse("2020-07-01"),
+        prisonId = prisonId,
+        prisonName = "$prisonId prison",
+        cellLocation = "1-1-004",
+        locationDescription = "$prisonId prison",
+        alerts = emptyList(),
+      ),
+      OffenderSearchPrisoner(
+        prisonerNumber = "A1234AE",
+        bookingId = "1234138",
+        firstName = "PAUL",
+        middleNames = null,
+        lastName = "RUDD",
+        status = "ACTIVE IN",
+        inOutStatus = "IN",
+        dateOfBirth = LocalDate.parse("1970-03-04"),
+        receptionDate = LocalDate.parse("2020-07-01"),
+        prisonId = prisonId,
+        prisonName = "$prisonId prison",
+        cellLocation = "1-1-005",
+        locationDescription = "$prisonId prison",
+        alerts = emptyList(),
+      ),
+      // </editor-fold>
+    )
     stubFor(
       get("/prison/$prisonId/prisoners").willReturn(
         aResponse()
           .withHeader("Content-Type", "application/json")
           .withBody(
             mapper.writeValueAsBytes(
-              OffenderSearchPrisonerList(
-                totalElements = 2,
-                content = listOf(
-                  OffenderSearchPrisoner(
-                    prisonerNumber = "A1409AE",
-                    bookingId = "110001",
-                    firstName = "JAMES",
-                    middleNames = null,
-                    lastName = "HALLS",
-                    status = "ACTIVE IN",
-                    inOutStatus = "IN",
-                    dateOfBirth = LocalDate.parse("1971-07-01"),
-                    receptionDate = LocalDate.parse("2020-07-01"),
-                    prisonId = prisonId,
-                    prisonName = "$prisonId prison",
-                    cellLocation = "2-1-002",
-                    locationDescription = "$prisonId prison",
-                    alerts = listOf(
-                      OffenderSearchPrisonerAlert(
-                        alertType = "H",
-                        alertCode = "HA",
-                        active = true,
-                        expired = false,
-                      ),
-                    ),
-                  ),
-                  OffenderSearchPrisoner(
-                    prisonerNumber = "G6123VU",
-                    bookingId = "110002",
-                    firstName = "RHYS",
-                    middleNames = "BARRY",
-                    lastName = "JONES",
-                    status = "ACTIVE IN",
-                    inOutStatus = "IN",
-                    dateOfBirth = LocalDate.parse("1970-03-01"),
-                    receptionDate = LocalDate.parse("2020-07-01"),
-                    prisonId = prisonId,
-                    prisonName = "$prisonId prison",
-                    cellLocation = "2-1-003",
-                    locationDescription = "$prisonId prison",
-                    alerts = listOf(),
-                  ),
-                ),
-              )
+              OffenderSearchPrisonerList(totalElements = offenders.size, content = offenders)
             )
           )
       )
