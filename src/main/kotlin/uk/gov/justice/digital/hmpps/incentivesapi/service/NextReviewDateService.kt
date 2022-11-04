8bf20214c66e0d4faabb838d7158d9d9e5f88bbb
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2022-11-04 11:39:08 +0000

    INC-896: Added "young people" exception for next review when on Basic
    > [...] if not suitable to return to Standard level further reviews must
    > be undertaken at least every 28 days thereafter.
    >
    > Exception for those identified as at Risk of Suicide and Self-harm (ACCT)
    > and for young people, where further reviews must be undertaken at least
    > every 14 days thereafter
    
    This change adds this exception for "young people" to the 'Basic' rule.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/NextReviewDateService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/NextReviewDateService.kt
@@ -28,19 +28,21 @@ class NextReviewDateService(private val input: NextReviewDateInput) {
   }
 
   private fun rulesForBasic(): LocalDate {
+    val lastReviewDate = lastReviewDate()
+
     // "if not suitable to return to Standard level further reviews must be undertaken at least every 28 days thereafter"
     if (wasConfirmedBasic()) {
       // "Exception for those identified as at Risk of Suicide and Self-harm (ACCT) and for young people,
       // where further reviews must be undertaken at least every 14 days thereafter"
-      if (input.hasAcctOpen) {
-        return lastReviewDate().plusDays(14)
+      if (input.hasAcctOpen || wasYoungPersonOnDate(lastReviewDate)) {
+        return lastReviewDate.plusDays(14)
       }
 
-      return lastReviewDate().plusDays(28)
+      return lastReviewDate.plusDays(28)
     }
 
     // "PF 5.16 Prisoners placed on Basic must be reviewed within 7 days"
-    return lastReviewDate().plusDays(7)
+    return lastReviewDate.plusDays(7)
   }
 
   private fun ruleForNewPrisoners(): LocalDate {
