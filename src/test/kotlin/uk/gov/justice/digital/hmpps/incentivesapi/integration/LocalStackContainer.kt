37a3f7a02ff2781e55111ae30f37099b1599d888
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-11-24 17:04:54 +0000

    Upgrade localstack version

--- src/test/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/integration/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/integration/LocalStackContainer.kt
@@ -24,7 +24,7 @@ object LocalStackContainer {
     if (localstackIsRunning()) return null
     val logConsumer = Slf4jLogConsumer(log).withPrefix("localstack")
     return LocalStackContainer(
-      DockerImageName.parse("localstack/localstack").withTag("0.14.0")
+      DockerImageName.parse("localstack/localstack").withTag("1.2")
     ).apply {
       withServices(LocalStackContainer.Service.SQS, LocalStackContainer.Service.SNS)
       withEnv("HOSTNAME_EXTERNAL", "localhost")
