0580d46e52d49879de554582d0e7e4efb48c9ebf
Aldo Giambelluca <xoen@users.noreply.github.com>
2023-01-17 16:09:14 +0000

    INC-988: Cache global list of all incentive levels (#342)
    * INC-988: Updated `getIepLevels()` to return a List (not a Flow)
    
    The client code using this always convert it to a List, so it makes sense
    to return a simple List.
    
    * INC-988: Cache the global list of all incentive levels
    
    This practically almost never change and we use it in quite a few places
    so it's wasteful to make an HTTP request every time we need this list.
    
    I tried to use Spring Boot's caching mechanism but for some reason I
    couldn't get it to work - maybe is the combination of libraries we use?
    
    The value is cached for 24 hours in memory, it's very simple but it should
    be good enough for this use case.
    
    * INC-988: Make it look like Kotlin
    
    Using Elvis operator

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/PrisonApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/PrisonApiService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.incentivesapi.service
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
@@ -16,12 +17,21 @@ import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonapi.PrisonLocation
 import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonapi.PrisonerAtLocation
 import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonapi.PrisonerExtraInfo
 import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonapi.ProvenAdjudication
+import uk.gov.justice.digital.hmpps.incentivesapi.util.CachedValue
+import java.time.Clock
 
 @Service
 class PrisonApiService(
   private val prisonWebClient: WebClient,
   private val prisonWebClientClientCredentials: WebClient,
+  clock: Clock,
 ) {
+
+  val allIncentiveLevelsCache = CachedValue<List<IepLevel>>(validForHours = 24, clock)
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
 
   private fun getClient(useClientCredentials: Boolean = false): WebClient {
     return if (useClientCredentials) prisonWebClientClientCredentials else prisonWebClient
@@ -35,14 +45,16 @@ class PrisonApiService(
   }
 
   suspend fun getIncentiveLevels(): Map<String, IepLevel> {
-    return getIepLevels().toList().associateBy { iep -> iep.iepLevel }
+    return getIepLevels().associateBy { iep -> iep.iepLevel }
   }
 
-  fun getIepLevels(): Flow<IepLevel> {
-    return getClient(true)
+  suspend fun getIepLevels(): List<IepLevel> = allIncentiveLevelsCache.get() ?: run {
+    log.debug("Getting all incentive levels using GET /api/reference-domains/domains/IEP_LEVEL/codes...")
+    val newValue = getClient(true)
       .get()
       .uri("/api/reference-domains/domains/IEP_LEVEL/codes")
-      .retrieve().bodyToFlow<IncentiveLevel>()
+      .retrieve()
+      .bodyToFlow<IncentiveLevel>()
       .map {
         IepLevel(
           iepLevel = it.code,
@@ -51,6 +63,11 @@ class PrisonApiService(
           active = it.activeFlag == "Y"
         )
       }
+      .toList()
+
+    allIncentiveLevelsCache.update(newValue)
+
+    return newValue
   }
 
   fun retrieveCaseNoteCounts(type: String, offenderNos: List<String>): Flow<CaseNoteUsage> =
