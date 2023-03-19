10e14f1bb79a61eceb9bd026d1518ace157c5166
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-03-19 16:42:47 +0000

    Mark required fields on incentive and prison incentive levels … fixing `PUT` operations that require a complete payload even for non-nullable Int fields

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/dto/PrisonIncentiveLevel.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/dto/PrisonIncentiveLevel.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.incentivesapi.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 /**
@@ -7,28 +8,39 @@ import io.swagger.v3.oas.annotations.media.Schema
  */
 data class PrisonIncentiveLevel(
   @Schema(description = "The incentive level code this refers to", example = "STD", minLength = 1, maxLength = 6)
+  @JsonProperty(required = true)
   val levelCode: String,
   @Schema(description = "The incentive level this refers to", example = "Standard", minLength = 1, readOnly = true)
+  @JsonProperty(required = false, access = JsonProperty.Access.READ_ONLY)
   val levelDescription: String = "",
   @Schema(description = "The prison this refers to", example = "MDI", minLength = 1, maxLength = 6)
+  @JsonProperty(required = true)
   val prisonId: String,
   @Schema(description = "Indicates that this incentive level is enabled in this prison", example = "true", defaultValue = "true")
+  @JsonProperty(required = false, defaultValue = "true")
   val active: Boolean = true,
   @Schema(description = "Indicates that this incentive level is the default for new admissions", example = "true", defaultValue = "false")
+  @JsonProperty(required = false, defaultValue = "false")
   val defaultOnAdmission: Boolean = false,
 
   @Schema(description = "The amount transferred weekly from the private cash account to the spends account for a remand prisoner to use", example = "5500", minimum = "0", type = "integer", format = "int32")
+  @JsonProperty(required = true)
   val remandTransferLimitInPence: Int,
   @Schema(description = "The maximum amount allowed in the spends account for a remand prisoner", example = "55000", minimum = "0", type = "integer", format = "int32")
+  @JsonProperty(required = true)
   val remandSpendLimitInPence: Int,
   @Schema(description = "The amount transferred weekly from the private cash account to the spends account for a convicted prisoner to use", example = "1800", minimum = "0", type = "integer", format = "int32")
+  @JsonProperty(required = true)
   val convictedTransferLimitInPence: Int,
   @Schema(description = "The maximum amount allowed in the spends account for a convicted prisoner", example = "18000", minimum = "0", type = "integer", format = "int32")
+  @JsonProperty(required = true)
   val convictedSpendLimitInPence: Int,
 
   @Schema(description = "The number of weekday visits for a convicted prisoner", example = "2", minimum = "0", type = "integer", format = "int32")
+  @JsonProperty(required = true)
   val visitOrders: Int,
   @Schema(description = "The number of privileged/weekend visits for a convicted prisoner", example = "1", minimum = "0", type = "integer", format = "int32")
+  @JsonProperty(required = true)
   val privilegedVisitOrders: Int,
 ) {
   fun toUpdate() = PrisonIncentiveLevelUpdate(
