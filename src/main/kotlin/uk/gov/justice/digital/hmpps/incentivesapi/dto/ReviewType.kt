cb321fdb93ebd8317b40e74b6c4d63e4e83d04c4
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2023-03-28 14:26:56 +0100

    Update `IsRealReview` so that `reviewType` is not nullable
    `IsRealReview` is an interface implemented by `IepDetail` (dto for data
    that was coming from NOMIS/Prison API) and `PrisonerIepLevel` (data coming
    from our database).
    
    Now that `reviewType` is non-nullable in `IepDetail` we can simplify
    `IsRealReview` as it doesn't need to consider `null` values anymore.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/dto/ReviewType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/dto/ReviewType.kt
@@ -5,13 +5,12 @@ enum class ReviewType {
 }
 
 interface IsRealReview {
-  val reviewType: ReviewType?
+  val reviewType: ReviewType
 
   fun isRealReview(): Boolean {
-    // NOTE: Reviews from NOMIS wouldn't have a review type.
-    // We consider these and `MIGRATED` reviews as "real" as we don't have a way to discriminate
+    // We consider `MIGRATED`/historic reviews as "real" as we don't have a way to discriminate
     // real incentives reviews in these cases.
     // Eventually all reviews will come from our DB and have a "review type".
-    return reviewType == null || listOf(ReviewType.REVIEW, ReviewType.MIGRATED).contains(reviewType)
+    return listOf(ReviewType.REVIEW, ReviewType.MIGRATED).contains(reviewType)
   }
 }
