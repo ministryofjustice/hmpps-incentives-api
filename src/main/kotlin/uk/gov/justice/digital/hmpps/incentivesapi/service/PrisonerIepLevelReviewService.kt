a50d2931412f685eee8b49824192cb15339ba3bb
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2023-04-11 11:43:06 +0100

    Removed unused import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/PrisonerIepLevelReviewService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/PrisonerIepLevelReviewService.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.incentivesapi.service
 
-import jakarta.validation.ValidationException
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.count
