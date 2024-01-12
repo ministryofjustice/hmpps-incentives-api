164916b53d9077ef2987b2edb0578f8f96740346
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-01-12 10:40:55 +0000

    Move to localstack v3 for testing as required by `hmpps-spring-boot-sqs`

--- src/test/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/integration/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/integration/LocalStackContainer.kt
@@ -27,7 +27,7 @@ object LocalStackContainer {
     if (localstackIsRunning()) return null
     val logConsumer = Slf4jLogConsumer(log).withPrefix("localstack")
     return LocalStackContainer(
-      DockerImageName.parse("localstack/localstack").withTag("1.3.1"),
+      DockerImageName.parse("localstack/localstack").withTag("3"),
     ).apply {
       withServices(LocalStackContainer.Service.SQS, LocalStackContainer.Service.SNS)
       withEnv("DEFAULT_REGION", "eu-west-2")
