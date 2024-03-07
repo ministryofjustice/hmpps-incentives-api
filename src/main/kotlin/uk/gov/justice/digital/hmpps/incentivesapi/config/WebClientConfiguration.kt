bf750ed026536313361df228d43e49311d3ca8a4
mjwillis <michael.willis@digital.justice.gov.uk>
2024-03-07 18:49:19 +0000

    Update WebClient configurations
    This commit modifies the WebClient configuration to include response timeout and alters the method used to construct the client. It also updates the application health timeout value from 20s to 2m and replaces "prison-api" with a system username in the authorized client manager.
    
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/WebClientConfiguration.kt
@@ -3,8 +3,12 @@ package uk.gov.justice.digital.hmpps.incentivesapi.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import uk.gov.justice.digital.hmpps.incentivesapi.SYSTEM_USERNAME
 import uk.gov.justice.hmpps.kotlin.auth.reactiveAuthorisedWebClient
 import uk.gov.justice.hmpps.kotlin.auth.reactiveHealthWebClient
 import java.time.Duration
@@ -14,19 +18,25 @@ class WebClientConfiguration(
   @Value("\${api.base.url.oauth}") val authBaseUri: String,
   @Value("\${api.base.url.prison}") private val prisonRootUri: String,
   @Value("\${api.base.url.offender-search}") private val offenderSearchUri: String,
-  @Value("\${api.timeout:20s}") val healthTimeout: Duration,
+  @Value("\${api.timeout:2m}") val healthTimeout: Duration,
 ) {
 
   @Bean
   fun authWebClient(builder: WebClient.Builder): WebClient = builder.reactiveHealthWebClient(authBaseUri, healthTimeout)
 
   @Bean
-  fun prisonWebClient(reactiveAuthorizedClientManager: ReactiveOAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
-    builder.reactiveAuthorisedWebClient(reactiveAuthorizedClientManager, registrationId = "prison-api", url = prisonRootUri, healthTimeout)
+  fun prisonWebClient(): WebClient {
+    val httpClient = HttpClient.create().responseTimeout(healthTimeout)
+    return WebClient.builder()
+      .baseUrl(prisonRootUri)
+      .clientConnector(ReactorClientHttpConnector(httpClient))
+      .filter(AuthTokenFilterFunction())
+      .build()
+  }
 
   @Bean
   fun prisonWebClientClientCredentials(reactiveAuthorizedClientManager: ReactiveOAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
-    builder.reactiveAuthorisedWebClient(reactiveAuthorizedClientManager, registrationId = "prison-api", url = prisonRootUri, healthTimeout)
+    builder.reactiveAuthorisedWebClient(reactiveAuthorizedClientManager, registrationId = SYSTEM_USERNAME, url = prisonRootUri, healthTimeout)
 
   @Bean
   fun prisonHealthWebClient(builder: WebClient.Builder): WebClient = builder.reactiveHealthWebClient(prisonRootUri, healthTimeout)
@@ -36,5 +46,6 @@ class WebClientConfiguration(
 
   @Bean
   fun offenderSearchWebClient(reactiveAuthorizedClientManager: ReactiveOAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
-    builder.reactiveAuthorisedWebClient(reactiveAuthorizedClientManager, registrationId = "offender-search", url = offenderSearchUri, healthTimeout)
+    builder.reactiveAuthorisedWebClient(reactiveAuthorizedClientManager, registrationId = SYSTEM_USERNAME, url = offenderSearchUri, healthTimeout)
+
 }
