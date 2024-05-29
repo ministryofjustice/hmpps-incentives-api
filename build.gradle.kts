8abd279e7738db20e29d9d2d4da91ce10821633b
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-05-29 10:19:40 +0100

    Finish upgrade to HMPPS gradle spring boot 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
   runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("org.flywaydb:flyway-database-postgresql")
   runtimeOnly("org.postgresql:r2dbc-postgresql")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql")
