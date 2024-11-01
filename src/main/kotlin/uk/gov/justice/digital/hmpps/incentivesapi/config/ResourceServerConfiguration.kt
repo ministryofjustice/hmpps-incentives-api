659d6e7269125c494b31e6ff238d8467efb0dc31
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-11-01 12:02:15 +0000

    IR-565 Fix DLQ retry issue

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/ResourceServerConfiguration.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.incentivesapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomizer
+
+@Configuration
+class ResourceServerConfiguration {
+
+  @Bean
+  fun resourceServerCustomizer() = ResourceServerConfigurationCustomizer {
+    unauthorizedRequestPaths {
+      addPaths = setOf(
+        "/queue-admin/retry-all-dlqs",
+      )
+    }
+  }
+}
