fb2477cf299c32b74f82fc826e5d71a2523a5201
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-12-17 15:58:31 +0000

    Specify Europe/London timezone rather than defaulting to system

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/ClockConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/ClockConfiguration.kt
@@ -3,11 +3,12 @@ package uk.gov.justice.digital.hmpps.incentivesapi.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Clock
+import java.time.ZoneId
 
 @Configuration
-class ClockConfig {
+class ClockConfiguration(
+  private val zoneId: ZoneId,
+) {
   @Bean
-  fun clock(): Clock {
-    return Clock.systemDefaultZone()
-  }
+  fun clock(): Clock = Clock.system(zoneId)
 }
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/TimeZoneIdConfiguration.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.incentivesapi.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.ZoneId
+
+@Configuration
+class TimeZoneIdConfiguration(
+  @Value("\${spring.jackson.time-zone}")
+  private val timeZone: String,
+) {
+  @Bean
+  fun timeZoneId(): ZoneId = ZoneId.of(timeZone)
+}
