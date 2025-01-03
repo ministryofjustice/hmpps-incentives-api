9a9ddb648eca58c44b88ef237924f10fae0c5cc9
Igor <igor.ushkarev@digital.justice.gov.uk>
2025-01-03 12:57:22 +0000

    Keep kotlin plugins < 2.1 since dependencies are still incompatible

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import uk.gov.justice.digital.hmpps.gradle.RevealSecretsTask
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.2"
-  kotlin("plugin.jpa") version "2.1.0"
-  kotlin("plugin.spring") version "2.1.0"
+  kotlin("plugin.jpa") version "2.0.21"
+  kotlin("plugin.spring") version "2.0.21"
   id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
   id("jacoco")
   id("org.sonarqube") version "6.0.1.5171"
