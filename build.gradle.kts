b7db83aa6a00c57c37b83e7e037e7813ffb9fb18
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-05-20 11:47:53 +0100

    IR-1053 Update Kotlin and SonarQube plugins, and upgrade OpenTelemetry instrumentation dependency (#669)
    * IR-1053 Update Kotlin and SonarQube plugins, and upgrade OpenTelemetry instrumentation dependency
    
    * Update Kotlin and Swagger parser dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ import uk.gov.justice.digital.hmpps.gradle.RevealSecretsTask
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
-  kotlin("plugin.jpa") version "2.1.20"
-  kotlin("plugin.spring") version "2.1.20"
+  kotlin("plugin.jpa") version "2.1.21"
+  kotlin("plugin.spring") version "2.1.21"
   id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
   id("jacoco")
-  id("org.sonarqube") version "6.1.0.5360"
+  id("org.sonarqube") version "6.2.0.5505"
 }
 
 dependencyCheck {
@@ -53,7 +53,7 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("io.opentelemetry:opentelemetry-api:1.50.0")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.15.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.16.0")
 
   implementation("org.apache.commons:commons-lang3")
   implementation("org.apache.commons:commons-text:1.13.1")
@@ -67,7 +67,7 @@ dependencies {
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.3")
   testImplementation("org.awaitility:awaitility-kotlin")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.27")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.28")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.wiremock:wiremock-standalone:3.13.0")
   testImplementation("org.testcontainers:localstack:1.21.0")
