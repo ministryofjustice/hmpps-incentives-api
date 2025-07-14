ministryofjustice/hmpps-incentives-api

commit 04b5c6bd069e38e6300a0f02c53d6f99190f587f

Limit fields returned by prisoner search for smaller requests (#681)
* Limit fields returned by offender search for smaller requests
* Rename internal references to match prisoner search (which used to be called offender search)
* Simplify tests by reusing one function to create mock prisoners

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/PrisonerSearchService.kt
@@ -1,0 +1,66 @@
+package uk.gov.justice.digital.hmpps.incentivesapi.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonersearch.PageOfPrisoners
+import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonersearch.Prisoner
+
+@Service
+class PrisonerSearchService(
+  private val prisonerSearchWebClient: WebClient,
+  objectMapper: ObjectMapper,
+) {
+  private val responseFields by lazy {
+    objectMapper.serializerProviderInstance.findValueSerializer(Prisoner::class.java).properties()
+      .asSequence()
+      .joinToString(",") { it.name }
+  }
+
+  /**
+   * Searches for prisoners in a prison using an optional cell location prefix (e.g. MDI-1)
+   * returning a flow of pages.
+   * Requires role PRISONER_IN_PRISON_SEARCH or PRISONER_SEARCH
+   */
+  fun findPrisonersAtLocation(prisonId: String, cellLocationPrefix: String = ""): Flow<List<Prisoner>> = flow {
+    var page = 0
+    do {
+      val pageOfPrisoners = prisonerSearchWebClient.get()
+        .uri(
+          "/prison/{prisonId}/prisoners?" +
+            "responseFields={responseFields}&" +
+            "cellLocationPrefix={cellLocationPrefix}&" +
+            "size={size}&page={page}&sort={sort}",
+          mapOf(
+            "prisonId" to prisonId,
+            "responseFields" to responseFields,
+            "cellLocationPrefix" to cellLocationPrefix,
+            // NB: API allows up 3,000 results per page
+            "size" to 500,
+            "page" to page,
+            "sort" to "prisonerNumber,ASC",
+          ),
+        )
+        .retrieve()
+        .awaitBody<PageOfPrisoners>()
+      emit(pageOfPrisoners.content)
+      page += 1
+    } while (!pageOfPrisoners.last)
+  }
+
+  /**
+   * Searches for prisoners in a prison using an optional cell location prefix (e.g. MDI-1)
+   * returning a complete list.
+   * Requires role PRISONER_IN_PRISON_SEARCH or PRISONER_SEARCH
+   */
+  suspend fun getPrisonersAtLocation(prisonId: String, cellLocationPrefix: String = ""): List<Prisoner> {
+    val prisoners = mutableListOf<Prisoner>()
+    findPrisonersAtLocation(prisonId, cellLocationPrefix).collect {
+      prisoners.addAll(it)
+    }
+    return prisoners
+  }
+}

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/WebClientConfiguration.kt
@@ -16,7 +16,7 @@ import java.time.Duration
 class WebClientConfiguration(
   @param:Value($$"${api.base.url.oauth}") val authBaseUri: String,
   @param:Value($$"${api.base.url.prison}") private val prisonRootUri: String,
-  @param:Value($$"${api.base.url.offender-search}") private val offenderSearchUri: String,
+  @param:Value($$"${api.base.url.prisoner-search}") private val prisonerSearchUri: String,
   @param:Value($$"${api.base.url.locations}") private val locationsUri: String,
   @param:Value($$"${api.timeout:2m}") val healthTimeout: Duration,
 ) {
@@ -65,17 +65,17 @@ class WebClientConfiguration(
     builder.reactiveHealthWebClient(locationsUri, healthTimeout)
 
   @Bean
-  fun offenderSearchHealthWebClient(builder: WebClient.Builder): WebClient =
-    builder.reactiveHealthWebClient(offenderSearchUri, healthTimeout)
+  fun prisonerSearchHealthWebClient(builder: WebClient.Builder): WebClient =
+    builder.reactiveHealthWebClient(prisonerSearchUri, healthTimeout)
 
   @Bean
-  fun offenderSearchWebClient(
+  fun prisonerSearchWebClient(
     reactiveAuthorizedClientManager: ReactiveOAuth2AuthorizedClientManager,
     builder: WebClient.Builder,
   ): WebClient = builder.reactiveAuthorisedWebClient(
     reactiveAuthorizedClientManager,
     registrationId = SYSTEM_USERNAME,
-    url = offenderSearchUri,
+    url = prisonerSearchUri,
     healthTimeout,
   )
 }

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/dto/prisonersearch/PrisonerSearch.kt
@@ -1,9 +1,10 @@
-package uk.gov.justice.digital.hmpps.incentivesapi.dto
+package uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonersearch
+
 import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonapi.PrisonerAlert
 import uk.gov.justice.digital.hmpps.incentivesapi.service.PrisonerInfoForNextReviewDate
 import java.time.LocalDate
 
-data class OffenderSearchPrisoner(
+data class Prisoner(
   override val bookingId: Long,
   override val prisonerNumber: String,
   override val dateOfBirth: LocalDate,
@@ -17,8 +18,8 @@ data class OffenderSearchPrisoner(
   override val hasAcctOpen = alerts.any(PrisonerAlert::isOpenAcct)
 }
 
-data class OffenderSearchPrisonerList(
-  val content: List<OffenderSearchPrisoner>,
+data class PageOfPrisoners(
+  val content: List<Prisoner>,
   val totalElements: Int,
   val last: Boolean,
 )

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/health/HealthPingCheck.kt
@@ -15,9 +15,9 @@ class PrisonHealthPing(
   @Qualifier("prisonHealthWebClient") webClient: WebClient,
 ) : ReactiveHealthPingCheck(webClient)
 
-@Component("offenderSearch")
-class OffenderSearchHealthPing(
-  @Qualifier("offenderSearchHealthWebClient") webClient: WebClient,
+@Component("prisonerSearch")
+class PrisonerSearchHealthPing(
+  @Qualifier("prisonerSearchHealthWebClient") webClient: WebClient,
 ) : ReactiveHealthPingCheck(webClient)
 
 @Component("locations")

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/BehaviourService.kt
@@ -17,7 +17,7 @@ class BehaviourService(
   private val behaviourCaseNoteMap = mapOf("POS" to "IEP_ENC", "NEG" to "IEP_WARN")
 
   suspend fun getBehaviours(reviews: List<IncentiveReview>): BehaviourSummary {
-    val lastRealReviews = getLastRealReviewForOffenders(reviews)
+    val lastRealReviews = getLastRealReviewForPrisoners(reviews)
     val lastReviewsOrDefaultPeriods = lastRealReviews.mapValues { truncateReviewDate(it.value) }
     val caseNoteCountsByType = getCaseNoteUsageByLastReviewDate(
       prisonApiService.retrieveCaseNoteCountsByFromDate(
@@ -45,7 +45,7 @@ class BehaviourService(
   private fun calcTypeCount(caseNoteUsage: List<PrisonerCaseNoteByTypeSubType>): Int =
     caseNoteUsage.sumOf { it.numCaseNotes }
 
-  private fun getLastRealReviewForOffenders(reviews: List<IncentiveReview>) = reviews
+  private fun getLastRealReviewForPrisoners(reviews: List<IncentiveReview>) = reviews
     .groupBy { it.bookingId }
     .mapValues { review ->
       val latestRealReview = review.value.firstOrNull(IncentiveReview::isRealReview)

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/CountPrisonersService.kt
@@ -8,16 +8,16 @@ import uk.gov.justice.digital.hmpps.incentivesapi.jpa.repository.IncentiveReview
 @Service
 class CountPrisonersService(
   private val incentiveReviewRepository: IncentiveReviewRepository,
-  private val offenderSearchService: OffenderSearchService,
+  private val prisonerSearchService: PrisonerSearchService,
 ) {
   /**
    * True if there are currently any prisoners on a given incentive level at a prison
    */
   suspend fun prisonersExistOnLevelInPrison(prisonId: String, levelCode: String): Boolean {
     var prisonersExistOnLevel = false
-    offenderSearchService.findOffendersAtLocation(prisonId)
-      .takeWhile { offenders ->
-        val bookingIds = offenders.map { it.bookingId }
+    prisonerSearchService.findPrisonersAtLocation(prisonId)
+      .takeWhile { prisoners ->
+        val bookingIds = prisoners.map { it.bookingId }
         prisonersExistOnLevel = if (bookingIds.isEmpty()) {
           false
         } else {

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/IncentiveReviewsService.kt
@@ -11,7 +11,7 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import uk.gov.justice.digital.hmpps.incentivesapi.config.ListOfDataNotFoundException
 import uk.gov.justice.digital.hmpps.incentivesapi.dto.IncentiveReviewLevel
 import uk.gov.justice.digital.hmpps.incentivesapi.dto.IncentiveReviewResponse
-import uk.gov.justice.digital.hmpps.incentivesapi.dto.OffenderSearchPrisoner
+import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonersearch.Prisoner
 import uk.gov.justice.digital.hmpps.incentivesapi.jpa.IncentiveReview
 import uk.gov.justice.digital.hmpps.incentivesapi.jpa.repository.IncentiveReviewRepository
 import uk.gov.justice.digital.hmpps.incentivesapi.util.flow.toMap
@@ -24,7 +24,7 @@ import uk.gov.justice.digital.hmpps.incentivesapi.dto.IncentiveReview as Incenti
 
 @Service
 class IncentiveReviewsService(
-  private val offenderSearchService: OffenderSearchService,
+  private val prisonerSearchService: PrisonerSearchService,
   private val locationsService: LocationsService,
   private val prisonIncentiveLevelService: PrisonIncentiveLevelAuditedService,
   private val incentiveReviewRepository: IncentiveReviewRepository,
@@ -49,9 +49,9 @@ class IncentiveReviewsService(
     page: Int = 0,
     size: Int = DEFAULT_PAGE_SIZE,
   ): IncentiveReviewResponse = coroutineScope {
-    val deferredOffenders = async {
-      // all offenders at location are required to determine total number with overdue reviews
-      offenderSearchService.getOffendersAtLocation(prisonId, cellLocationPrefix)
+    val deferredPrisoners = async {
+      // all prisoners at location are required to determine total number with overdue reviews
+      prisonerSearchService.getPrisonersAtLocation(prisonId, cellLocationPrefix)
     }
     val deferredLocationDescription = async {
       try {
@@ -64,16 +64,16 @@ class IncentiveReviewsService(
       }
     }
 
-    val offenders = deferredOffenders.await()
+    val prisoners = deferredPrisoners.await()
 
-    val bookingIds = offenders.map(OffenderSearchPrisoner::bookingId)
+    val bookingIds = prisoners.map(Prisoner::bookingId)
 
     val deferredBehaviourCaseNotesSinceLastReview = async {
       val reviews = incentiveReviewRepository.findAllByBookingIdInOrderByReviewTimeDesc(bookingIds = bookingIds)
       behaviourService.getBehaviours(reviews.toList())
     }
-    val deferredIncentiveLevels = async { getIncentiveLevelsForOffenders(bookingIds) }
-    val deferredNextReviewDates = async { nextReviewDateGetterService.getMany(offenders) }
+    val deferredIncentiveLevels = async { getIncentiveLevelsForPrisoners(bookingIds) }
+    val deferredNextReviewDates = async { nextReviewDateGetterService.getMany(prisoners) }
 
     val incentiveLevels = deferredIncentiveLevels.await()
     val bookingIdsMissingIncentiveLevel = bookingIds subtract incentiveLevels.keys
@@ -89,7 +89,7 @@ class IncentiveReviewsService(
       lastRealReviewDate?.let { ChronoUnit.DAYS.between(it.toLocalDate(), today).toInt() }
     }
 
-    val allReviews = offenders
+    val allReviews = prisoners
       .map {
         IncentiveReviewDTO(
           prisonerNumber = it.prisonerNumber,
@@ -151,7 +151,7 @@ class IncentiveReviewsService(
     )
   }
 
-  private suspend fun getIncentiveLevelsForOffenders(bookingIds: List<Long>): Map<Long, IncentiveReview> =
+  private suspend fun getIncentiveLevelsForPrisoners(bookingIds: List<Long>): Map<Long, IncentiveReview> =
     incentiveReviewRepository.findAllByBookingIdInAndCurrentIsTrueOrderByReviewTimeDesc(bookingIds)
       .toMap(IncentiveReview::bookingId)
 }

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/KpiService.kt
@@ -5,8 +5,8 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.count
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.incentivesapi.dto.OffenderSearchPrisoner
 import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonapi.Prison
+import uk.gov.justice.digital.hmpps.incentivesapi.dto.prisonersearch.Prisoner
 import uk.gov.justice.digital.hmpps.incentivesapi.jpa.repository.KpiRepository
 import uk.gov.justice.digital.hmpps.incentivesapi.jpa.repository.ReviewsConductedPrisonersReviewed
 import java.time.LocalDate
@@ -15,7 +15,7 @@ import java.time.LocalDate
 class KpiService(
   private val kpiRepository: KpiRepository,
   private val prisonApiService: PrisonApiService,
-  private val offenderSearchService: OffenderSearchService,
+  private val prisonerSearchService: PrisonerSearchService,
 ) {
 
   suspend fun getNumberOfReviewsConductedAndPrisonersReviewed(day: LocalDate): ReviewsConductedPrisonersReviewed {
@@ -28,7 +28,7 @@ class KpiService(
     getPrisons().map { prisonId ->
       async { getPrisonersInPrison(prisonId) }
     }.awaitAll().forEach { prisoners ->
-      allPrisoners.addAll(prisoners.map(OffenderSearchPrisoner::prisonerNumber))
+      allPrisoners.addAll(prisoners.map(Prisoner::prisonerNumber))
     }
 
     println("Number of prisoners across the estate: ${allPrisoners.size}")
@@ -46,8 +46,8 @@ class KpiService(
       .map(Prison::agencyId)
   }
 
-  private suspend fun getPrisonersInPrison(prisonId: String): List<OffenderSearchPrisoner> {
+  private suspend fun getPrisonersInPrison(prisonId: String): List<Prisoner> {
     println("Getting list of prisoners for $prisonId...")
-    return offenderSearchService.getOffendersAtLocation(prisonId)
+    return prisonerSearchService.getPrisonersAtLocation(prisonId)
   }
 }

commit 31927eb5c288023184e459f727f86d567af7e86c

Fix to request only real prisoner search fields (#682)
Bug introduced in 04b5c6bd069e38e6300a0f02c53d6f99190f587f

src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/service/PrisonerSearchService.kt
@@ -17,6 +17,7 @@ class PrisonerSearchService(
   private val responseFields by lazy {
     objectMapper.serializerProviderInstance.findValueSerializer(Prisoner::class.java).properties()
       .asSequence()
+      .filterNot { it.name == "hasAcctOpen" }
       .joinToString(",") { it.name }
   }
 