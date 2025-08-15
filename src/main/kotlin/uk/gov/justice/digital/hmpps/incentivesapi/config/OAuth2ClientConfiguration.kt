84f6e8b323024dae5db6bf8585685f7aa396ac01
Michael Willis <michael.willis2@justice.gov.uk>
2025-08-15 21:28:36 +0100

    INC-1398 Add OAuth2 client caching mechanism and configuration to optimize authorized client management. (#694)
    * INC-1398 Add OAuth2 client caching mechanism and configuration to optimize authorized client management.
    
    * INC-1398 Enable reactive support for OAuth2 client management by refactoring configuration and caching service.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/OAuth2ClientConfiguration.kt
@@ -0,0 +1,26 @@
+package uk.gov.justice.digital.hmpps.incentivesapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+
+@Configuration
+class OAuth2ClientConfiguration {
+
+  @Bean
+  fun authorizedClientManager(
+    clientRegistrationRepository: ReactiveClientRegistrationRepository,
+  ): ReactiveOAuth2AuthorizedClientManager {
+    val authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
+    val authorizedClientManager =
+      AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+        clientRegistrationRepository,
+        ClientCachingOAuth2AuthorizedClientService(clientRegistrationRepository),
+      )
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+    return authorizedClientManager
+  }
+}
