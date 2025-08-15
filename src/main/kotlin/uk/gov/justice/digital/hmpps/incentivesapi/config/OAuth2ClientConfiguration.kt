e2ad9c4d22bafb142a85d15b5814ef96382fca40
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-08-15 16:52:52 +0100

    INC-1398 Enable reactive support for OAuth2 client management by refactoring configuration and caching service.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/OAuth2ClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incentivesapi/config/OAuth2ClientConfiguration.kt
@@ -2,21 +2,21 @@ package uk.gov.justice.digital.hmpps.incentivesapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 
 @Configuration
 class OAuth2ClientConfiguration {
 
   @Bean
   fun authorizedClientManager(
-    clientRegistrationRepository: ClientRegistrationRepository,
-  ): OAuth2AuthorizedClientManager {
-    val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
+    clientRegistrationRepository: ReactiveClientRegistrationRepository,
+  ): ReactiveOAuth2AuthorizedClientManager {
+    val authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
     val authorizedClientManager =
-      AuthorizedClientServiceOAuth2AuthorizedClientManager(
+      AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
         clientRegistrationRepository,
         ClientCachingOAuth2AuthorizedClientService(clientRegistrationRepository),
       )
