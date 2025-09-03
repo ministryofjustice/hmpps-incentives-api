9ecd65c8532401b9cbc369644cfaf155c81ebb15
Michael Willis <michael.willis2@justice.gov.uk>
2025-09-03 13:46:10 +0100

    INC-1400 Replace custom OAuth2 client caching service with shared ReactiveGlobalPrincipalOAuth2AuthorizedClientService. Upgrade hmpps-kotlin-spring-boot-starter to 1.5.1-beta. (#705)

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ repositories {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.1-beta")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.10")
